2adcb155b9d4322aed58ca534d0654ff31686ee1
DUBAIL Fabien <fdubail@iocean.fr>
2017-08-09 15:10:56 +0200

    TP-01

---
+++ src/main/resources/fr/iocean/dta/App.java
@@ -0,0 +1,10 @@
+package fr.iocean.dta;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class App {
+
+}
